dc4864e2754896d80dc0b3cf8bf7a7d4dc33dff4
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-05-21 14:01:25 +0300

    making history

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,25 @@ import Phonebook from "components/Phonebook/Phonebook";
 import Contacts from "components/Contacts/Contacts";
 import Filter from "components/Filter/Filter";
 
+const CB_KEY = "contact";
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      let contactPars = JSON.stringify(this.state.contacts);
+      localStorage.setItem(CB_KEY, contactPars);
+    }
+  }
+  componentDidMount() {
+    let contactBook = JSON.parse(localStorage.getItem(CB_KEY));
+    this.setState({
+      contacts: contactBook,
+    });
+  }
   addContact = ({ name, number }) => {
     const conatactId = nanoid();
     const newContact = {
